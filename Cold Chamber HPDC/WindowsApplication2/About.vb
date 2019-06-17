07bc11ef54a9524c7b4cffeab0058e8cb3295795
Xecus <abo.elfotouh@live.com>
2019-06-17 22:13:16 +0200

    Initiate and add First Release

---
+++ Cold Chamber HPDC/WindowsApplication2/About.vb
@@ -0,0 +1,10 @@
+﻿Public Class About
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Closebutton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
---
+++ Cold Chamber HPDC/WindowsApplication2/Doutput.vb
@@ -0,0 +1,37 @@
+﻿Public Class Doutput
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Close_Click(sender As Object, e As EventArgs) Handles CloseB.Click
+        Me.Hide()
+        OverA.Text = ""
+        OverB.Text = ""
+        OverC.Text = ""
+        OverV.Text = ""
+        Vent.Text = ""
+        WrT.Text = ""
+        ArT.Text = ""
+        HrT.Text = ""
+        DsT.Text = ""
+
+        Me.Close()
+    End Sub
+
+    Private Sub Doutput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label44_Click(sender As Object, e As EventArgs) Handles Label44.Click
+
+    End Sub
+
+    Private Sub Label46_Click(sender As Object, e As EventArgs) Handles Label46.Click
+
+    End Sub
+End Class
